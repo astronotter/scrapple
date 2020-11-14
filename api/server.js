efda83234149bb2665f8525f221c3aa7d1296b9c
astronotter <ottersocks@outlook.com>
2020-11-14 10:22:11 -0500

    Add fs

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const https = require('https')
 const cors = require('cors')
+const fs = require('fs')
 const { Sequelize, DataTypes, Utils } = require('sequelize')
 
 const PORT = process.env.PORT || 3000
